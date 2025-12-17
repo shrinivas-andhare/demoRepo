394039f7c08e312811535a66b4dbaccc775bd27a
shrinivas-andhare <andhareshri@gmail.com>
2025-12-18 01:18:47 +0530

    changes are done

--- Frist.java
+++ Frist.java
@@ -3,6 +3,8 @@ public interface Frist {
 	
 	public void show();
 
-	System.out.println("This Must To Print On A Console")
+	System.out.println("This Must To Print On A Console");
+	
+	System.out.print("First Proj from Eclips");
 
 }
